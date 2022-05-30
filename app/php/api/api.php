65a3c7179d379e033f963e7adb657ec7fa3ddaee
AquaUseful <l.vladik.m@gmail.com>
2022-05-30 23:01:23 +0300

    Some namespaces refactoring

--- app/php/api/api.php
+++ app/php/api/api.php
@@ -2,46 +2,43 @@
 
 namespace api\api {
     require_once('../utils/utils.php');
+    require_once('../utils/session.php');
     require_once('handlers/game.php');
     require_once('handlers/page.php');
+    use utils;
+    use api;
 
-    use function utils\utils\filter_request_method;
-    use function utils\utils\send_error;
-    use api\game;
-    use api\page;
-
-    filter_request_method('POST');
+    utils\utils\filter_request_method('POST');
     process_request();
-
-function get_response_template(): array
-{
-    return [
-        'ok' => false,
-        'err' => '',
-        'content' => array()
-    ];
-}
+    function construct_response(bool $ok, string $err, array $content): array
+    {
+        return [
+            'ok' => $ok,
+            'err' => $err,
+            'content' => $content
+        ];
+    }
     function process_request(): void
     {
         $req = get_json_request();
         if (!check_request($req)) {
-            send_error(400, 'Bad formed request structure');
+            utils\utils\send_error(400, 'Bad formed request structure');
             exit();
         }
         $response = array();
         switch ($req['api']) {
             case 'game':
-                $response = game\handle_request($req);
+                $response = api\game\handle_request($req);
                 break;
 
             case 'page':
-                $response = page\handle_request($req);
+                $response = api\page\handle_request($req);
                 break;
-            
+
             default:
-                send_error(400, 'Invalid api');
+                utils\utils\send_error(400, 'Invalid api');
                 exit();
-            break;
+                break;
         }
         send_json_response($response);
     }
@@ -59,11 +56,11 @@ function get_response_template(): array
             array_key_exists('action', $request) &&
             array_key_exists('content', $request);
         /* if (!$keys) {
-             return false;
+         return false;
          }*/
         $types = is_string($request['api']) &&
-        is_string($request['action']) &&
-        is_array($request['content']);
+            is_string($request['action']) &&
+            is_array($request['content']);
         return $types;
     }
 
@@ -72,7 +69,7 @@ function get_response_template(): array
         $text = file_get_contents('php://input');
         $req = json_decode($text, true, 512, JSON_UNESCAPED_UNICODE);
         if ($req === null) {
-            send_error(400, 'Error parsing json');
+            utils\utils\send_error(400, 'Error parsing json');
             exit();
         }
         return $req;
