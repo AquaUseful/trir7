c0f739d26f55bbab336eb7225a27dca13f2aa204
AquaUseful <l.vladik.m@gmail.com>
2022-06-02 18:53:03 +0300

    Logout button

--- app/js/header.js
+++ app/js/header.js
@@ -2,6 +2,7 @@ import { call_api } from "./utils.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
     await update_login();
+    await set_logout_btn();
 });
 
 async function update_login() {
@@ -11,4 +12,11 @@ async function update_login() {
     } else if (resp['err'] === 'nologin') {
         document.location.replace('index.html');
     }
+}
+
+async function set_logout_btn() {
+    document.getElementById('header_logout_btn').addEventListener('click', async () => {
+        await call_api('session', 'logout', {});
+        document.location.replace('index.html');
+    });
 }
